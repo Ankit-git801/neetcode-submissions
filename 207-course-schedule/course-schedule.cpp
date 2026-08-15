a1e51049a97aeb4bc03040c5db038c0a2033e80b
Ankit Kumar <ak759359@gmail.com>
2026-08-16 02:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.22%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-bool dfs(vector<vector<int>>&adj,int u,vector<bool>&vis,vector<bool>&cur){
-    vis[u]=true;
-    cur[u]=true;
-    for(int &x:adj[u]){
-        if(!vis[x]){
-            if(!dfs(adj,x,vis,cur))
-            return false;
-        }
-        else{
-            //cycle found
-            if(cur[x]==true)return false;
-        }
-    }
-    cur[u]=false; 
-    return true;
-}
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(numCourses,0);
         vector<vector<int>>adj(numCourses);
         for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        vector<bool>vis(numCourses,false);
-        vector<bool>cur(numCourses,false);
-        for(int i=0;i<numCourses;i++){
-            if(!dfs(adj,i,vis,cur))return false;
+        for(int i=0;i<adj.size();i++){
+            for(int &u:adj[i])indegree[u]++;
         }
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++)if(indegree[i]==0)q.push(i);
+        vector<int>ans;
+        while(!q.empty()){
+            int cur=q.front();
+            q.pop();
+            ans.push_back(cur);
+            for(int &x:adj[cur]){
+                indegree[x]--;
+                if(indegree[x]==0)q.push(x);
+            }
+        }
+        if(ans.size()!=numCourses)return false;
         return true;
     }
 };
